5e8bc0adccd4671cd536892fb059880a4c6eba9b
Miguel Garcia <miguelgarcia@pursuit.org>
2021-06-15 21:44:35 -0400

    transactionsController exported

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,6 +1,9 @@
 const transactionsController = require("express").Router();
 const transactionsArr = require("../models/transactionsArr");
 
+// localhost:xxxx/transactions
 transactionsController.get("/", (req, res) => {
     res.json(transactionsArr);
 });
+
+module.exports = transactionsController
