e07d2c519185906b1cfa3cd2bf0f61234a0316c2
Miguel Garcia <miguelgarcia@pursuit.org>
2021-06-16 15:04:35 -0400

    + all /transactions routes

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,12 +1,20 @@
 const transactionsController = require("express").Router();
 const transactionsArr = require("../models/transactionsArr");
 
-// localhost:xxxx/transactions
+// localhost:xxxx/transactions <index>
 transactionsController.get("/", (req, res) => {
   res.json(transactionsArr);
 });
 
-// localhost:xxxx/transactions/:index
+// localhost:xxxx/transactions <create>
+transactionsController.post("/", (req, res) => {
+  const { body } = req;
+  transactionsArr.push(body);
+  const newIndex = transactionsArr.length - 1;
+  res.json(transactionsArr[newIndex]);
+});
+
+// localhost:xxxx/transactions/:index <show>
 transactionsController.get("/:index", (req, res) => {
   const { index } = req.params;
   const transaction = transactionsArr[index];
@@ -15,6 +23,21 @@ transactionsController.get("/:index", (req, res) => {
   } else {
     res.redirect("/404");
   }
+});
+
+// localhost:xxxx/transactions/:index <update>
+transactionsController.put("/:index", (req, res) => {
+  const { index } = req.params;
+  const { body } = req;
+  transactionsArr[index] = body;
+  res.json(transactionsArr[index]);
+});
+
+// localhost:xxxx/transactions/:index <destroy>
+transactionsController.delete("/:index", (req, res) => {
+  const { index } = req.params;
+  const deletedTransaction = transactionsArr.splice(index, 1);
+  res.json(deletedTransaction);
 });
 
 module.exports = transactionsController;
